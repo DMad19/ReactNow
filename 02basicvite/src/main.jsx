611ecb4744fd5aa09a62cc81f67115b272d12c72
DMad19 <dineshmadduru98813@gmail.com>
2024-01-07 23:17:33 +0530

    fourth commit

--- 02basicvite/src/main.jsx
+++ 02basicvite/src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+// import App from './App.jsx'
 
-
+const aReactElement = React.createElement(
+  "a",
+  {
+    href: "https://google.com",
+    target: "_blank",
+  },
+  "click me to go google",
+)
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  aReactElement
 )
