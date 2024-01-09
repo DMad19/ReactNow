020373f9be8be65a8ad7426e36276c92d3989669
DMad19 <dineshmadduru98813@gmail.com>
2024-01-09 23:22:45 +0530

    04tailwindprops added

---
+++ 04TailwindProps/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Card from './components/Card'
+function App() {
+
+  return (
+    <>
+      <Card userName='Dinesh' role='AllRounder' source='https://images.pexels.com/photos/9420724/pexels-photo-9420724.jpeg?auto=compress&cs=tinysrgb&w=600'/>
+      <Card />
+      <Card/>
+    </>
+  )
+}
+
+export default App
